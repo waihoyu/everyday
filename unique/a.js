239fc6c168483f1f212f7e018f6b3d600e91cf00
waihoyu <13701343809@qq.com>
2018-05-09 21:22:00 +0800

    '代码更新'

---
+++ unique/a.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, 3, 4, 1, 4, 5, 3];
+const arr2 = [];
+
+function unique(arr) {
+    if (!Array.isArray(arr)) {
+        console.log("type error")
+        return;
+    } else {
+        arr.forEach(element => {
+            if (arr2.indexOf(element) < 0) {
+                arr2.push(element)
+            }
+        })
+    }
+    return arr2;
+}
+
+console.log(unique(arr));
