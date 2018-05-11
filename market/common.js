46c7bb3f1a04ed6e7d4d0c789b3d9675e610fe52
waihoyu <13701343809@qq.com>
2018-05-11 20:39:25 +0800

    '更新'

--- market/common.js
+++ market/common.js
@@ -1,8 +1,10 @@
 window.onload = function() {
+
     let focus = document.getElementsByClassName('lnk-doubanapp');
     let disp = document.getElementsByClassName('more-items');
     let maru = document.getElementsByClassName('market-user');
     let navl = this.document.getElementsByClassName('nav-list');
+
     focus[0].onmouseover = function() {
         disp[0].style.display = "block";
         document.getElementsByClassName("top-nav-doubanapp")[0].classList.add("more-active");
@@ -28,5 +30,19 @@ window.onload = function() {
     maru[0].onmouseout = function() {
         navl[0].style.display = "none";
     }
+    let sliderInner = this.document.getElementsByClassName('slide-inner');
+    let counter = 0;
+
+    let spitem = document.getElementsByClassName('slide-paginator-item');
+    spitem[0].style.color = 'red';
+
+    setInterval(() => {
+        console.log(sliderInner[0].style.left + " " + counter);
+        spitem[counter].style.color = 'red';
+        sliderInner[0].style.left = counter++ > 5 ? sliderInner[0].style.left = counter = 0 : parseInt(sliderInner[0].style.left) - 1903 + 'px';
+        // let spitem = document.getElementsByClassName('slide-paginator-item');
+        // counter++;
+
+    }, 2000);
 
 }
