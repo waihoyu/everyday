a54e661a0cd3555e66ee76ed903a46195ec34276
waihoyu <13701343809@qq.com>
2018-05-18 15:32:10 +0800

    '代码提交'

--- vue/index.js
+++ vue/index.js
@@ -3,11 +3,17 @@ new Vue({
     data: {
         name: "测试1",
         website: 'www.baidu.com',
-        websiteTag: '<div>test</dive'
+        websiteTag: '<div>test</dive',
+        x: 0,
+        y: 0
     },
     methods: {
         greet: function(t) {
             return "GOOD" + t;
+        },
+        getmouseXY: function(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 });
