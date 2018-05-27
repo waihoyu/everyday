9e1312a5361f89998e3287321038fec8d3056dde
waihoyu <13701343809@qq.com>
2018-05-27 15:40:58 +0800

    '代码更新'

---
+++ es6/js基础使用.js
@@ -0,0 +1,20 @@
+/**
+ * js 的基础使用
+ */
+// 四种for循环
+// let data = { name: "Jim", age: 20 };
+// for (const iterator of Object.keys(data)) {
+//     console.log(iterator);
+// }
+// filter的使用
+// let temp = [1, 2, 3, 4, 5].filter(function(item) {
+//     if (item < 4) {
+//         return item;
+//     }
+// });
+// console.log(temp);
+// 字符串模板的使用
+// let tempArray = [1, 2, 3].map(function(item) {
+//     return `<li>${item}</li>`
+// })
+// console.log(tempArray.join(""));
