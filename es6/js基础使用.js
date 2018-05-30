048a70e5e1f8caee8a04a19d637250f505c5a3cd
waihoyu <13701343809@qq.com>
2018-05-30 14:43:15 +0800

    '代码更新'

--- es6/js基础使用.js
+++ es6/js基础使用.js
@@ -3,19 +3,32 @@
  *vscode ide 真的很好用呢
  */
 // 四种for循环
-// let data = { name: "Jim", age: 20 };
+// let data = { name: "Jim", age: 20 }
 // for (const iterator of Object.keys(data)) {
-//     console.log(iterator);
+//     console.log(iterator)
 // }
 // filter的使用
 // let temp = [1, 2, 3, 4, 5].filter(function(item) {
 //     if (item < 4) {
-//         return item;
+//         return item
 //     }
-// });
-// console.log(temp);
+// })
+// console.log(temp)
 // 字符串模板的使用
 // let tempArray = [1, 2, 3].map(function(item) {
 //     return `<li>${item}</li>`
 // })
-// console.log(tempArray.join(""));
+// console.log(tempArray.join(""))
+
+let a = 0;
+
+/**
+ * 
+ * 
+ * @param {any} b 
+ * @param {any} c 
+ */
+
+function a(b, c) {
+    return
+}
