834f6a561a30adcd61cf78d97c259bff8b8a40f2
waihoyu <13701343809@qq.com>
2018-06-03 23:34:54 +0800

    '代码更新'

---
+++ es6/数组的使用.js
@@ -0,0 +1,2 @@
+let aa = [1, 2, 34];
+console.log(aa.includes(2));
---
+++ es6/箭头函数.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: WaiHoYu 
+ * @Date: 2018-06-03 22:16:23 
+ * @Last Modified by:   WaiHoYu 
+ * @Last Modified time: 2018-06-03 22:16:231 
+ * @Description:     
+ */
+
+
+let a = () => {
+
+    console.log("箭头函数");
+}
+
+a();
