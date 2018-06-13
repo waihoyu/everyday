fc5fc02db47f18bcbe99124281bd52d538079c9c
waihoyu <13701343809@qq.com>
2018-06-13 16:29:31 +0800

    '代码更新'

---
+++ es6/symbol/tostring.js
@@ -0,0 +1,23 @@
+const arr = [1, 2, 3];
+console.log(arr instanceof Array);
+console.log(arr.toString());
+console.log(typeof arr);
+console.log(Object.prototype.toString.call(arr));
+const zk = {
+    name: "曾凯"
+}
+zk.__proto__.toString = function() {
+    for (var key in this) {
+        console.log(key + ":" + this[key]);
+    }
+}
+zk.toString()
+
+console.log(zk.toString());
+// 序列化  "{name:"曾凯",habit:"交友"}"
+// console.log(zk.toString());
+console.log(JSON.stringify(zk));
+/* 
+object 来自 function(){}
+构造函数有prototype属性  对象的__proto__
+*/
