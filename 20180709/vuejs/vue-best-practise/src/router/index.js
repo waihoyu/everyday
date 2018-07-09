1e83bf6db1d66f7010e52fcb7eebc4ea9e5dee72
waihoyu <13701343809@qq.com>
2018-07-09 19:57:05 +0800

    learn eslint demo

---
+++ 20180709/vuejs/vue-best-practise/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import NotFound from '@/components/NotFound'
+import TimeEntries from '@/components/TimeEntries'
+import LogTime from '@/components/LogTime'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      // name: 'Home',
+      component: Home
+    },
+    {
+      path: '/home',
+      // name: 'Home',
+      component: Home
+    },
+    {
+      path: '/time-entries',
+      // name: 'Home',
+      component: TimeEntries,
+      children:
+      [
+        {
+          path: 'log-time',
+          component: LogTime
+        }]
+    },
+    {
+      path: '*',
+      // name: 'Home',
+      component: NotFound
+    }
+  ]
+})
